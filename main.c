707bec06dbf7d8a384bb54cf895d5d145c52267c
Fredrik <s185130@student.dtud.k>
2019-04-12 10:36:51 +0200

    aaa

--- main.c
+++ main.c
@@ -1,2 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
 
-#include "main.h"
+int main() {
+    int n = scanf("%d");
+    int arr[n*4];
+    for (int j = 0; j < sizeof(arr); j++) {
+        printf("Please enter a number: ");
+        scanf("%d", &arr[j]);
+        if (arr[j] == 0) {
+            break;
+        }
+    }
+
+
+    return 0;
+
+}
