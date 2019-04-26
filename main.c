202a565f4690211279b0d2822445448fd51aeb92
Mads Møller Jensen <mads1606@gmail.com>
2019-04-26 10:06:59 +0200

    implemented search tree methods   TODO fix insert method

--- main.c
+++ main.c
@@ -11,101 +11,146 @@ struct roof {
     int endY;
 
     int waterCollected;
+
+    int key;
+    struct roof *parent, *left, *right;
 };
 
+//roof methods
 void printRoof(struct roof roof);
-void sortRoofArray(struct roof roofs[], int size);
-int compareRoofs (struct roof r1, struct roof r2);
+int getMax(struct roof r);
+
+//binary search methods
+void setKey(struct roof *r);
+struct roof * predecessor (int key, struct roof *root);
+struct roof * successor (int key, struct roof *root);
+struct roof * insert (struct roof *r, struct roof *root);
+void delete (struct roof *r, struct roof *root);
+void printInOrder (struct roof *root);
 
 int main() {
     int n;
     scanf("%d", &n);
-    struct roof roofs[n];
+    struct roof *roofPtr, *root;
+    root = NULL;
 
-    struct roof *roofPtr;
-    for (int j = 0; j < sizeof(roofs) / sizeof(roofs[0]); j++) {
+    for (int j = 0; j < n; j++) {
         roofPtr = malloc(sizeof(roofPtr));
         scanf("%d %d %d %d", &roofPtr->startX, &roofPtr->startY, &roofPtr->endX, &roofPtr->endY);
         roofPtr->waterCollected = -1;
-        roofs[j] = *roofPtr;
+
+        roofPtr->parent = NULL; roofPtr->left = NULL; roofPtr->right = NULL;
+        setKey(roofPtr);
+        insert(roofPtr, root);
     }
 
-    for (int j = 0; j < sizeof(roofs) / sizeof(roofs[0]); j++) {
-        printRoof(roofs[j]);
-    }
+    printInOrder(root);
     printf("%s", "\n\n__________________________________\n");
 
-    sortRoofArray(roofs, n);
-
-    printf("%s", "\n\n_______________FINAL________________\n");
-    for (int j = 0; j < sizeof(roofs) / sizeof(roofs[0]); j++) {
-        printRoof(roofs[j]);
-    }
 
     return 0;
 
 }
 
+
+/*
+ **************    roof methods    ************
+ */
+/**
+ * print the data of a roof in an easy to read way
+ * @param roof  the roof being printed
+ */
 void printRoof (struct roof roof) {
     printf("(%d, %d), (%d, %d)\n", roof.startX, roof.startY, roof.endX, roof.endY);
 }
 
-void sortRoofArray(struct roof roofs[], int size){
-    int min;
-    int max;
-    int middle;
+/**
+ * Returns the greatest height of the roof
+ */
+int getMax(struct roof r) {
+    int rMax;
 
-    for (int i = 1; i < size; i++) {
-        min = 0;
-        max = i;
-        middle = ((max - min) / 2) + min;
-        while (true) {
+    if (r.startY > r.endY)
+        rMax = r.startY;
+    else
+        rMax = r.endY;
 
-            /*if (compareRoofs(roofs[max], roofs[i]) >= 0) {
-                struct roof temp = roofs[i];
-                for (int j = i; j > max; j--){
-                    roofs[j] = roofs[j-1];
-                }
-                roofs[max + 1] = temp;
-                break;
-            }*/
-            if (min == middle){
-                if (compareRoofs(roofs[middle], roofs[i]) > 0)
-                    middle++;
-                struct roof temp = roofs[i];
-                for (int j = i; j > middle; j--){
-                    roofs[j] = roofs[j-1];
-                }
-                roofs[middle] = temp;
-                break;
-            }
-            if (compareRoofs(roofs[middle], roofs[i]) > 0) {
-                min = middle;
-                middle = ((max - min + 1) / 2) + min;
-            } else {
-                max = middle;
-                middle = ((max - min) / 2) + min;
-            }
 
-        }
+    return rMax;
+}
 
+
+
+/*
+ *************    b-tree methods     ****************
+ */
+
+/**
+ * Automatically sets the key according to our convention
+ * @param r the roof getting it's key set
+ */
+void setKey(struct roof *r){
+    r->key = getMax(*r);
+}
+
+/**
+ *
+ * @param key the key of which one wishes to find the predecessor
+ * @param root the root of the search tree
+ * @return the element with the greatest key smaller than @param key
+ */
+struct roof * predecessor (int key, struct roof *root){
+    if (root == NULL) return NULL;
+    if (root->key == key) return root;
+    if (key < root->key) return predecessor(key, root->left);
+    struct roof *t = predecessor(key, root->right);
+    return (t != NULL ? t : root); //return t or root
+}
+
+
+/**
+ *
+ * @param key the key of which one wishes to find the predecessor
+ * @param root the root of the search tree
+ * @return the element with the smallest key greater than @param key
+ */
+struct roof * successor (int key, struct roof *root){
+    if (root == NULL) return NULL;
+    if (root->key == key) return root;
+    if (key > root->key) return predecessor(key, root->right);
+    struct roof *t = successor(key, root->left);
+    return (t != NULL ? t : root); //return t or root
+}
+
+/**
+ * inserts a new roof in the correct place
+ * @param r the roof to be inserted
+ * @param root the root of the search tree
+ * @return void
+ */
+struct roof * insert (struct roof *r, struct roof *root) {
+    if (root == NULL) {
+        root = r;
+        return r;
     }
+    if (r->key > root->key)
+        root->left = insert(r, root->right);
+    else
+        root->right = insert(r, root->left);
 
 }
 
-int compareRoofs (struct roof r1, struct roof r2) {
-    int r1Max;
-    int r2Max;
+void delete (struct roof *r, struct roof *root){
+    //TODO implement
+}
 
-    if (r1.startY > r1.endY)
-        r1Max = r1.startY;
-    else
-        r1Max = r1.endY;
-
-    if (r2.startY > r2.endY)
-        r2Max = r2.startY;
-    else
-        r2Max = r2.endY;
-
-    return r2Max - r1Max;
+/**
+ * Prints all roofs in ascending order of keys
+ * @param root the root of the tree or sub-tree being printed
+ */
+void printInOrder (struct roof *root){
+    if (root == NULL) return;
+    printInOrder(predecessor(root->key, root));
+    printRoof(*root);
+    printInOrder(successor(root->key, root));
 }
