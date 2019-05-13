0a5ae2c341b6b852ebf32e70be9e3281ecab61b8
Mads Møller Jensen <mads1606@gmail.com>
2019-05-13 18:08:35 +0200

    added files

---
+++ cmake-build-debug/CMakeFiles/Assignment3_C.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/c/Users/Bruger/Google Drev/DTU/2. semester/C_for_javaprogrammoerer/Projects/C_Assignment3/Assignment3_CProgramming-/Main.c" "/cygdrive/c/Users/Bruger/Google Drev/DTU/2. semester/C_for_javaprogrammoerer/Projects/C_Assignment3/Assignment3_CProgramming-/cmake-build-debug/CMakeFiles/Assignment3_C.dir/Main.c.o"
+  "/cygdrive/c/Users/Bruger/Google Drev/DTU/2. semester/C_for_javaprogrammoerer/Projects/C_Assignment3/Assignment3_CProgramming-/roofDataStructures/Roof.c" "/cygdrive/c/Users/Bruger/Google Drev/DTU/2. semester/C_for_javaprogrammoerer/Projects/C_Assignment3/Assignment3_CProgramming-/cmake-build-debug/CMakeFiles/Assignment3_C.dir/roofDataStructures/Roof.c.o"
+  "/cygdrive/c/Users/Bruger/Google Drev/DTU/2. semester/C_for_javaprogrammoerer/Projects/C_Assignment3/Assignment3_CProgramming-/roofDataStructures/RoofTree.c" "/cygdrive/c/Users/Bruger/Google Drev/DTU/2. semester/C_for_javaprogrammoerer/Projects/C_Assignment3/Assignment3_CProgramming-/cmake-build-debug/CMakeFiles/Assignment3_C.dir/roofDataStructures/RoofTree.c.o"
+  "/cygdrive/c/Users/Bruger/Google Drev/DTU/2. semester/C_for_javaprogrammoerer/Projects/C_Assignment3/Assignment3_CProgramming-/roofDataStructures/waterCalc.c" "/cygdrive/c/Users/Bruger/Google Drev/DTU/2. semester/C_for_javaprogrammoerer/Projects/C_Assignment3/Assignment3_CProgramming-/cmake-build-debug/CMakeFiles/Assignment3_C.dir/roofDataStructures/waterCalc.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Assignment3_C.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Assignment3_C.dir/Main.c.o"
+  "CMakeFiles/Assignment3_C.dir/roofDataStructures/RoofTree.c.o"
+  "CMakeFiles/Assignment3_C.dir/roofDataStructures/Roof.c.o"
+  "CMakeFiles/Assignment3_C.dir/roofDataStructures/waterCalc.c.o"
+  "Assignment3_C.pdb"
+  "Assignment3_C.exe"
+  "libAssignment3_C.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment3_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
